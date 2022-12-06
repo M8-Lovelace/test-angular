9b9676f84083a40f7d96c3c29dde0b689ab64e22
J6-Lovelace <lauritasusano.10@gmail.com>
2022-12-06 17:19:48 -0500

    chore: course progress up to video 9 with examples of events and dynamic properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'Laura';
+  age = 20;
+  img = 'https://source.unsplash.com/random';
+  btnDisable = true;
+  person = {
+    name : 'Edwin',
+    age : 23,
+    avatar :  'https://source.unsplash.com/random'
+  }
+
+  toggleButton(){
+    this.btnDisable = !this.btnDisable;
+  }
+
+  increadeAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event : Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
